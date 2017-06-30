b4faf02901a379cd676278d8b24da266c5cabe80
Ankit Vyas <ankitiphone85@gmail.com>
2017-06-30 12:17:08 +0530

    Removed Memory Related methods

--- ExercisesToGitHub/ViewController.swift
+++ ExercisesToGitHub/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Test Develope")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
